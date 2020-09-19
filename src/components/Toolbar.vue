39994966477aba8a11fadfed6bffd9e08c9a5a93
Andrea Ortiz <andrea.ortizg9@gmail.com>
2020-09-19 09:55:12 -0500

    first commit

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-app-bar
+      color="purple darken-2 "
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Fotos</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+<script>
+
+export default {
+  name:"Toolbar",
+
+};
+
+</script>
---
+++ src/components/listaimagenes.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-card>
+        <h1>Imágenes</h1>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name : 'ImageList' 
+}
+</script>
